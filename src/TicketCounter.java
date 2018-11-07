65348e8b6269e3553c34495ea53d4fd565ab2413
SMSHKR <39004788+SMSHKR@users.noreply.github.com>
2018-11-07 16:40:20 +0700

    Cleanup Code (3)

--- src/TicketCounter.java
+++ src/TicketCounter.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.lang.String;
@@ -6,13 +7,15 @@ import java.lang.String;
 class TicketCounter extends Thread {
     // Variable
     private File infile;
-    private int checkpoint, maxSeat;
+    private int checkpoint;
+    private int maxSeat;
+    private int transactionCount = 0;
 
     private ArrayList<BusLine> airport_bound = new ArrayList<>();
     private ArrayList<BusLine> city_bound = new ArrayList<>();
 
     // Constructor
-    public TicketCounter(String name, File input,int check,int max) {
+    public TicketCounter(String name, File input, int check, int max) {
         super(name);
         infile = input;
         maxSeat = max;
@@ -20,34 +23,30 @@ class TicketCounter extends Thread {
     }
 
     // Method
+    private void printAndCount(String nameOfTourGroup, String seat) {
+        System.out.printf("%s >> Transaction %d : %10s(%2s) bus \n",
+                Thread.currentThread().getName(), ++transactionCount, nameOfTourGroup, seat);
+    }
     @Override
     public void run() {
         try (Scanner scan = new Scanner(infile)) {
             while (scan.hasNext()) {
-                int i = 1;
-                String desNum;
-                String s = scan.nextLine();
-                String[] buf = s.split(",");
-                String des = buf[3].trim();
-                String seat  = buf[2].trim();
 
-                if ((des.equals("A"))) {
-                    System.out.printf("%s >> Transaction %d : %10s(%2s) bus \n",Thread.currentThread().getName(),i,buf[1].trim(),seat);
-                    i++;
-                } else {
-                    desNum=city_bound.get(i-1).allocateBus(seat,des);
-                    System.out.printf("%s >> Transaction %d %10s(%2s) bus \n", Thread.currentThread().getName(), i, buf[1].trim(),seat);
-                    i++;
+                String input           = scan.nextLine();
+                String [] buf          = input.split(",");
+                String nameOfTourGroup = buf[1].trim();
+                String seat            = buf[2].trim();
+                String destination     = buf[3].trim();
+
+                if ((destination.equals("A"))) {
+                    printAndCount(nameOfTourGroup, seat);
+                }
+                else {
+                    printAndCount(nameOfTourGroup, seat);
                 }
 
-                // if (i == checkpoint) f.await();
-                String name = buf[1];
-                //System.out.printf(name);
-                //System.out.printf("%s>> Transaction %d %s(%s) bus %s", Thread.currentThread().getName(), i, buf[1].trim(), m, buf[3].trim());
             }
-        } catch (Exception e) {
-
-        }
+        } catch (FileNotFoundException e) { e.printStackTrace(); }
     }
 
 }
